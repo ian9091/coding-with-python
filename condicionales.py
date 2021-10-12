370e8b9b680afa4aac25c1d10a55f2ed640cc87d
IvanGomez <ian9091@me.com>
2021-10-12 00:22:56 -0500

    Se añade código que ejemplifica el uso de "elif" que nos permite hacer cuestionamientos adicionales despues de ya haber colocado un if.

--- condicionales.py
+++ condicionales.py
@@ -1,5 +1,17 @@
-edad = int(float(input("Escribe tu edad: ")))
-if edad > 17: 
-    print("Eres mayor de edad. ")
-else :  
-    print("Eres menor de edad. ")
+# edad = int(float(input("Escribe tu edad: ")))
+# if edad > 17: 
+#     print("Eres mayor de edad. ")
+# else :  
+#     print("Eres menor de edad. ")
+
+numero = int(input("Escribe un número: "))
+
+if numero > 5 :
+    print("Número es mayor a 5")
+
+elif numero == 5 :
+    print("Número es igual a 5")
+
+else: 
+    print("Es menor a 5")
+    
