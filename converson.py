7aab280845e4fc92d185665d21100a61d7a49997
IvanGomez <ian9091@me.com>
2021-10-11 22:10:26 -0500

    Se remueve el archivo python cuyo nombre se encontraba incorrecto

--- converson.py
+++
@@ -1,6 +0,0 @@
-pesos = input("Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3753
-dolares = pesos / valor_dolar
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares") 
