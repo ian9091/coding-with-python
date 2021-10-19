61cc369fef87eff1039da6689e3e5b42fec52bed
IvanGomez <ian9091@me.com>
2021-10-18 23:00:51 -0500

    Se reescribe codigo con las mejoras que pretenden tomae en cuenta el resultado de las potencias mas no las repeticiones.

--- bucles.py
+++ bucles.py
@@ -1,17 +1,20 @@
 #Reto imprimir todas las potencias de 2 hasta mil
 
+def mensaje(cont,oper):
+    print('2 Elevado a ' + str(cont) + ' es igual a ' + str(oper))
 
-
-def run(num,rept):
-    if num <= rept:
-        contador = num
-        print('2 Elevado a ' + str(contador) + ' es igual a ' + str(2**contador))
-        run(num+1,rept)
+def run():
+    limite = int(input('Escriba el limite de la potencia de 2: '))
+    contador = 0
+    operacion = 2**contador
+    
+    if operacion <= limite:
+        mensaje(contador,operacion)
+        mensaje(contador+1,operacion)
     
     else: 
         print('Fin')
     
 
 if __name__ == '__main__':
-    limite = int(input('Escriba el limite de la potencia de 2: '))
-    run(0,limite)
+    run()
