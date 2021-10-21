6a1a1e2822a3c3725d40af9b4c82b5f316656aa1
IvanGomez <ian9091@me.com>
2021-10-20 23:14:41 -0500

    Se depura código comentado

--- bucles.py
+++ bucles.py
@@ -1,18 +1,6 @@
 #Este programa permite calcular las potencias de 2 hasta el 
 #resultado que definamos al inicio del programa. 
-
-#def operacion(cont):
- #   return 2**cont
-
-#def mensaje(cont,oper):
- #   print('2 Elevado a ' + str(cont) + ' es igual a ' + str(oper))
-
-#def bucle(contador,limite):
- #   if operacion(contador) <= limite:
-  #      mensaje(contador,operacion(contador))
-   #     bucle(contador+1,limite)
-    #else: 
-     #   print('Fin')
+#Usando el cicle while
 
 def run():
     LIMITE = int(input('Escriba el limite de la potencia de 2: '))
