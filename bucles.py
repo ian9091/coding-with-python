b9c61d4a45fd79341537ccef8d47382633647659
IvanGomez <ian9091@me.com>
2021-10-20 23:06:10 -0500

    Se simplifica el codigo utilizando el ciclo while

--- bucles.py
+++ bucles.py
@@ -1,24 +1,27 @@
 #Este programa permite calcular las potencias de 2 hasta el 
 #resultado que definamos al inicio del programa. 
 
-def operacion(cont):
-    return 2**cont
+#def operacion(cont):
+ #   return 2**cont
 
-def mensaje(cont,oper):
-    print('2 Elevado a ' + str(cont) + ' es igual a ' + str(oper))
+#def mensaje(cont,oper):
+ #   print('2 Elevado a ' + str(cont) + ' es igual a ' + str(oper))
 
-def bucle(contador,limite):
-    if operacion(contador) <= limite:
-        mensaje(contador,operacion(contador))
-        bucle(contador+1,limite)
-    else: 
-        print('Fin')
+#def bucle(contador,limite):
+ #   if operacion(contador) <= limite:
+  #      mensaje(contador,operacion(contador))
+   #     bucle(contador+1,limite)
+    #else: 
+     #   print('Fin')
 
 def run():
-    limite = int(input('Escriba el limite de la potencia de 2: '))
+    LIMITE = int(input('Escriba el limite de la potencia de 2: '))
     contador = 0
-    print('2 elevado a ' + str(contador) + ' es igual a ' + str(2**contador))
-    bucle(contador+1,limite)
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+        print('2 elevado a ' + str(contador) + ' es igual a ' + str(2**contador))
+        contador = contador + 1
+        potencia_2 = 2**contador
 
 
 if __name__ == '__main__':
