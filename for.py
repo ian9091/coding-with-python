73dda02e5b39ddc11902bc8a3693d349f31a1383
IvanGomez <ian9091@me.com>
2021-10-21 22:28:46 -0500

    Se continuan experimentando con el documento for.py

--- for.py
+++ for.py
@@ -1,5 +1,16 @@
-contador = 1
-print (contador)
-while contador < 1000:
-    contador = contador + 1
-    print(contador)
+#contador = 1
+#print (contador)
+#while contador < 1000:
+ #   contador += 1
+  #  print(contador)
+
+#for contador in range(1001):
+#    print(contador)
+#a = list(range(1001))
+#print(a)
+
+#for i in range(10):
+ #   print(11 * i)
+
+for a in range(30):
+    print(1 * a)
