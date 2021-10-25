aafc29621c96e90b61e653b67abb3e4c41040e2a
IvanGomez <ian9091@me.com>
2021-10-24 23:02:06 -0500

    Se experimenta con metodos alternos para hacer funcionar el programa.

--- prueba_primaridad.py
+++ prueba_primaridad.py
@@ -1,9 +1,16 @@
 def es_primo(numero):
     contador = 0
 
-    for i in range (1,numero + 1):
-        if i == 1 or i == numero:  
-            continue
+    #for i in range (1,numero + 1):
+     #   if i == 1 or i == numero:  
+      #      continue
+       # if numero % i == 0:
+        #    contador += 1
+        #if contador == 0:
+         #   return True
+        #else: 
+         #   return False
+    for i in list(2,3,5,7,11):
         if numero % i == 0:
             contador += 1
         if contador == 0:
