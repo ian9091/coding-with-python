c3ce9493654c6b08a095d4a06a1f28224b0e8e64
IvanGomez <ian9091@me.com>
2021-11-02 00:38:31 -0600

    Se le reelizan mejoras al juego de adivina el numero

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -4,20 +4,26 @@ def run():
     numero_random = random.randint(1,100)
 
     numero_elegido = int(input('Elige un numero entre el 1 y el 100: '))
-    creditos = 5
+    creditos = 7
     while numero_elegido != numero_random:
         if numero_elegido < numero_random:
             print('Elige un numero mayor')
             creditos -= 1
+            print('Te restan ',creditos, ' creditos')
         else:
             print('Busca un numero más pequeño')
             creditos -= 1
+            print('Te restan ',creditos, ' creditos')
+
         if creditos == 0:
+            print('Se te acabaron los itentos. Fin del Juego')
+            return creditos
             break
-        numero_elegido = int(input('Elige otro número: '))
-
-
-    print('¡Ganaste!')
+        else:
+            numero_elegido = int(input('Elige otro número: '))
+    if creditos != 0:
+        print('Ganaste')
+    
 
 
 if __name__ == '__main__':
